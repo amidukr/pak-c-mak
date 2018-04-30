24f60eef48ebd407679278d6d72a4f2c9350e4ce
Dmytro Brazhnyk <dmitry@brazhnik.org>
2018-05-01 00:09:38 +0300

    Module approach for commands.

--- packages/install-googletest.sh
+++
@@ -1,5 +0,0 @@
-function pak-install {
-	export CMAKE_ARGUMENTS="$CMAKE_ARGUMENTS -Dgtest_force_shared_crt=ON"
-
-	"$PAK_C_MAK_PATH/tools/git-cmake-install" googletest https://github.com/google/googletest master
-}

96435cb6465c6c851554e27f1487fc937bbb4036
Dmytro Brazhnyk <dmitry@brazhnik.org>
2018-05-04 23:31:00 +0300

    sync-repo command

--- repositories/packages/install-pak-c-mak-demo-app-with-deps.sh
+++
@@ -1,6 +0,0 @@
-function pak-install {
-    set -e
-	 
-    "$PAK_C_MAK_PATH/pak-c-mak" install googletest
-	"$PAK_C_MAK_PATH/pak-c-mak" install pak-c-mak-demo-app
-}
--- repositories/packages/install-pak-c-mak-demo-app.sh
+++
@@ -1,3 +0,0 @@
-function pak-install {
-	"$PAK_C_MAK_TOOLS_PATH/git-cmake-install" pak-c-mak-demo-app "https://github.com/amidukr/pak-c-mak-demo-app" master
-}
